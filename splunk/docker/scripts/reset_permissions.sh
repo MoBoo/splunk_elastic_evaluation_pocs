e21357bd4847ae5b656c3c73919f5439fa988718
MoBoo <moboo@online.de>
2020-10-22 15:01:12 +0200

    add exported saved-objects from kibana to use-case

--- splunk/docker/scripts/reset_permissions.sh
+++ splunk/docker/scripts/reset_permissions.sh
@@ -2,8 +2,12 @@
 # Splunks docker images are build using ansible and change permissions for mouted files.
 
 echo ">>> Reset Permissions for $(pwd)"
+echo ">>> Reset owner and group to mo:mo"
 chown -R mo:mo .
+
+echo ">>> Reset file and directory permissions"
 find . -type f -print0|xargs -0 chmod 664
 find . -type d -print0|xargs -0 chmod 775
 
+echo ">>> Make run.sh executeable again"
 chmod +x ./run.sh
