2e724e35620f4e2c6c387778981d3fc46d7cba8d
MoBoo <moboo@online.de>
2020-10-22 15:12:10 +0200

    add 'why is hardcoded user:groups required' in splunk/docker/scripts/reset-permissions.sh

--- splunk/docker/scripts/reset_permissions.sh
+++ splunk/docker/scripts/reset_permissions.sh
@@ -1,8 +1,15 @@
 # This script is used to restore default permissions and owners for splunk use cases.
 # Splunks docker images are build using ansible and change permissions for mouted files.
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
 echo ">>> Reset Permissions for $(pwd)"
 echo ">>> Reset owner and group to mo:mo"
+# User has to be hardcoded here because this script needs sudo to be able to change permissions.
 chown -R mo:mo .
 
 echo ">>> Reset file and directory permissions"
