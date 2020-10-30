84789d77ccca816424ac9730466ec2c3002ae20e
MoBoo <moboo@online.de>
2020-10-30 10:51:11 +0100

    update access.log to be the same across all PoCs;Create cluster test for elastic

---
+++ elastic/docker/cluster-test/run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Start docker container
+echo ">>> Starting docker environment."
+docker-compose up --build -d es-node-1 es-node-2 es-node-3 kibana
+
+# wait for elasticsearch to be available.
+echo ">>> Waiting for elasticsearch to become available. This may take a while."
+until $(curl --output /dev/null --silent --head --fail "localhost:9200"); do
+	echo -n "."
+	sleep 5
+done
+
+echo ""
+
+# wait for kibana to be available.
+echo ">>> Waiting for kibana to become available. This may take a while."
+until $(curl --output /dev/null --silent --head --fail "localhost:5601"); do
+        echo -n "."
+        sleep 5
+done
+
+echo ""
+
+if [ -d "./setup" ]; then
+	echo ">>> Running setup scripts."
+	for file in ./setup/*; do
+		echo ">>> Running: $file"
+		$file
+		sleep 1
+	done
+fi
+
+echo ""
+
+docker-compose up --build -d filebeat
+
+echo ">>> Setup completed. Start streaming docker logs. (Ctrl+C to exit.)"
+
+docker-compose logs -f
+
+echo ">>> Cleanung & exiting."
+
+# Wait for user input before stopping use-case.
+echo ">>> Stopping docker container."
+docker-compose stop
+
+echo ">>> Removing docker environment."
+docker-compose down -v
+rm -rf ./elasticsearch/**/data/*
+
+echo ">>> See you soon."
+
---
+++ elastic/docker/cluster-test/setup/01_create-index.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#echo ">>> Removing Index: http_access_logs"
+#/usr/bin/curl -X DELETE "localhost:9200/http_access_logs?pretty=true";echo
+
+echo ">>> Creating Index Template: http_access_logs_template"
+/usr/bin/curl -H "Content-Type: application/json" -X PUT "localhost:9200/_index_template/http_access_logs_template?pretty=true" -d '
+{
+"index_patterns": ["http_access_logs*"],
+  "template": {
+    "settings": {
+      "number_of_shards": 3,
+      "number_of_replicas": 2
+    },
+    "mappings": {
+      "properties": {
+        "client.address":{
+			"type":"keyword"
+		},
+		"client.ident":{
+			"type":"keyword"
+		},
+		"user.name":{
+			"type":"keyword"
+		},
+		"http.request.method":{
+			"type":"keyword"
+		},
+		"http.response.status_code":{
+			"type":"long"
+		},
+		"http.response.bytes":{
+			"type":"long"
+		},
+		"http.request.referrer":{
+			"type": "text",
+			"fields": {
+                          "keyword" : {
+				"type": "keyword"
+                          }
+			}
+		},
+		"user_agent.original":{
+			"type":"text",
+			"fields": {
+			  "keyword": {
+			  	"type": "keyword"
+			  }
+			}
+		},
+		"http.version":{
+			"type":"keyword"
+		},
+		"tags":{
+			"type":"keyword"
+		},
+		"geoip": {
+			"properties": {
+				"location": {"type": "geo_point" }
+			}
+		},
+                "timestamp":{
+			"type":"date",
+			"format":"dd/MMM/yyyy:HH:mm:ss"
+		}
+      }
+    },
+    "aliases": {
+      "http_access_logs_summary": { }
+    }
+  },
+  "_meta": {
+    "description": "Index Template for custom http_access_logs."
+  }
+}';echo
+
+echo ">>> Creating Index: http_access_logs"
+/usr/bin/curl -H "Content-Type: application/json" -X PUT "localhost:9200/http_access_logs?pretty=true";
+
