9eca92fc008c273a9b958560896821b8fb7bea2b
MoBoo <moboo@online.de>
2021-01-11 13:15:41 +0100

    Rename Directories to match thesis naming

--- elastic/docker/rally_performance_benchmark/setup/03_create_kibana_index_pattern.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ">>> Create kibana index-pattern: http_access_logs_pattern"
-curl -X POST "localhost:5601/api/saved_objects/index-pattern/http_access_logs_pattern"  -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '
-{
-  "attributes": {
-    "title": "http_access_logs*"
-  }
-}';echo
